3b1b5d319bda44a051d7628ab20e6875ec96db30
Vikash Sharma <vs8824394430@gmail.com>
2024-04-12 20:01:26 +0530

    create header and carddetails with router-dom

---
+++ src/components/CardDetails.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CardDetails = () => {
+  return <div>cardetails</div>;
+};
+
+export default CardDetails;
---
+++ src/components/Header.js
@@ -0,0 +1,60 @@
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import Badge from "@mui/material/Badge";
+import Nav from "react-bootstrap/Nav";
+import Menu from "@mui/material/Menu";
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+
+const Header = () => {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <>
+      <Navbar bg="dark" variant="dark" style={{ height: "60px" }}>
+        <Container>
+          <NavLink to="/" className="text-decoration-none text-light mx-3">
+            Add to Cart
+          </NavLink>
+          <Nav className="me-auto">
+            <NavLink to="/" className="text-decoration-none text-light">
+              Home
+            </NavLink>
+          </Nav>
+
+          <Badge
+            id="basic-button"
+            aria-controls={open ? "basic-menu" : undefined}
+            aria-haspopup="true"
+            aria-expanded={open ? "true" : undefined}
+            onClick={handleClick}
+          >
+            <i
+              class="fa-solid fa-cart-shopping text-light"
+              style={{ fontSize: 25, cursor: "pointer" }}
+            ></i>
+          </Badge>
+        </Container>
+
+        <Menu
+          id="basic-menu"
+          anchorEl={anchorEl}
+          open={open}
+          onClose={handleClose}
+          MenuListProps={{
+            "aria-labelledby": "basic-button",
+          }}
+        ></Menu>
+      </Navbar>
+    </>
+  );
+};
+
+export default Header;
